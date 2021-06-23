510bed5a53ca840dfe5482ee11e6c0eb336c36c4
David Wolfers <dsw247@cornell.edu>
2021-06-23 17:32:55 -0400

    Improved documentation slightly for resource allocation agents

--- or_suite/agents/resource_allocation/fixed_threshold.py
+++ or_suite/agents/resource_allocation/fixed_threshold.py
@@ -4,6 +4,8 @@ from .. import Agent
 
 
 ''' Agent which implements several heuristic algorithms'''
+
+
 class fixedThresholdAgent(Agent):
 
     def __init__(self, epLen, env_config):
@@ -18,40 +20,42 @@ class fixedThresholdAgent(Agent):
         self.num_resources = self.env_config['weight_matrix'].shape[1]
         self.budget_remaining = np.copy(self.env_config['init_budget'])
         #print('Starting Budget: ' + str(self.current_budget))
-        
+
         self.epLen = epLen
         self.data = []
         self.first_allocation_done = False
         self.exp_endowments, self.var_endowments = self.get_expected_endowments()
         self.prob, self.solver = self.generate_cvxpy_solver()
-        self.lower_sol = np.zeros((self.num_types,self.num_resources))
-        #print("R")
-        #print(self.rel_exp_endowments)
+        self.lower_sol = np.zeros((self.num_types, self.num_resources))
+        # print("R")
+        # print(self.rel_exp_endowments)
 
     def generate_cvxpy_solver(self):
         """
         Creates a generic solver to solve the offline resource allocation problem
-        
-        Inputs: 
-            num_types - number of types
-            num_resources - number of resources
+
+        Args: 
+            num_types: number of types
+            num_resources: number of resources
         Returns:
             prob - CVXPY problem object
             solver - function that solves the problem given data
         """
         num_types = self.num_types
         num_resources = self.num_resources
-        x = cp.Variable(shape=(num_types,num_resources))
+        x = cp.Variable(shape=(num_types, num_resources))
         sizes = cp.Parameter(num_types, nonneg=True)
         weights = cp.Parameter((num_types, num_resources), nonneg=True)
         budget = cp.Parameter(num_resources, nonneg=True)
-        objective = cp.Maximize(cp.log(cp.sum(cp.multiply(x, weights), axis=1)) @ sizes)
+        objective = cp.Maximize(
+            cp.log(cp.sum(cp.multiply(x, weights), axis=1)) @ sizes)
         constraints = []
         constraints += [0 <= x]
         for i in range(num_resources):
             constraints += [x[:, i] @ sizes <= budget[i]]
         # constraints += [x @ sizes <= budget]
         prob = cp.Problem(objective, constraints)
+
         def solver(true_sizes, true_weights, true_budget):
             sizes.value = true_sizes
             weights.value = true_weights
@@ -59,35 +63,34 @@ class fixedThresholdAgent(Agent):
             prob.solve()
             return prob.value, np.around(x.value, 5)
         return prob, solver
-    
-    def get_lower_upper_sol(self,init_sizes):
+
+    def get_lower_upper_sol(self, init_sizes):
         """
         uses solver to get the lower and upper
         """
         budget = self.env_config['init_budget']
         weights = self.env_config['weight_matrix']
         n = self.env_config['num_rounds']
-        tot_size =  np.sum(self.exp_endowments[:,1:], axis=1)
+        tot_size = np.sum(self.exp_endowments[:, 1:], axis=1)
         future_size = init_sizes + tot_size
 
-
-        conf_bnd = np.sqrt(np.max(self.var_endowments, axis=1)*np.mean(self.exp_endowments, axis=1)*(n-1))
-
+        conf_bnd = np.sqrt(np.max(self.var_endowments, axis=1)
+                           * np.mean(self.exp_endowments, axis=1)*(n-1))
 
         # print(future_size)
-        
-        lower_exp_size = future_size*(1 + np.max(np.sqrt(conf_bnd) / future_size))
+
+        lower_exp_size = future_size * \
+            (1 + np.max(np.sqrt(conf_bnd) / future_size))
         _, lower_sol = self.solver(lower_exp_size, weights, budget)
 
         #c = (1 / (n**(1/2)))*(1 +  np.max(np.sqrt(mean_size*n) / future_size)) -  np.max(np.sqrt(mean_size*n) / future_size)
-        #print(c)
+        # print(c)
         #upper_exp_size_12 = future_size*(1 - c)
         #_, upper_sol_12 = solver(upper_exp_size_12, weights, budget)
         # print('lower sol: ' + str(lower_sol))
         return lower_sol
 
-
-    def get_expected_endowments(self,N=1000):
+    def get_expected_endowments(self, N=1000):
         """
         Monte Carlo Method for estimating Expectation of type distribution using N realizations
         Only need to run this once to get expectations for all locations
@@ -111,19 +114,19 @@ class fixedThresholdAgent(Agent):
             var_size[:, t] = np.var(np.asarray(cur_list), axis=0)
         # print(exp_size)
         return exp_size, var_size
-        
+
     def reset(self):
         # resets data matrix to be empty
         self.current_budget = np.copy(self.env_config['init_budget'])
 
         self.data = []
-    
+
     def update_config(self, env, config):
         '''Updates environment configuration dictionary'''
         self.env_config = config
         return
 
-    def update_obs(self, obs, action, reward, newObs, timestep, info): 
+    def update_obs(self, obs, action, reward, newObs, timestep, info):
         '''Add observation to records'''
         self.data.append(newObs)
         return
@@ -133,29 +136,31 @@ class fixedThresholdAgent(Agent):
         self.current_budget = np.copy(self.env_config['init_budget'])
         self.greedy = self.greedy
 
-
     def greedy(self, state, timestep, epsilon=0):
         '''
         Select action according to function
         '''
-        #print("State:%s"%state)
+        # print("State:%s"%state)
         budget_remaining = state[:self.num_resources]
         sizes = state[self.num_resources:]
-        
+
         if not self.first_allocation_done:
             self.lower_sol = self.get_lower_upper_sol(sizes)
             self.first_allocation_done = True
             print('Lower Solutions:')
             print(self.lower_sol)
-        
-        lower_thresh = self.lower_sol
-        #print(lower_thresh.shape)
-        resource_index = budget_remaining - np.matmul(sizes, self.lower_sol) > 0
-        allocation= resource_index * lower_thresh + (1 - resource_index) * np.array([budget_remaining / np.sum(sizes),]*self.num_types)
-        self.budget_remaining -= budget_remaining - np.matmul(sizes, allocation)
-        
-        return allocation
 
+        lower_thresh = self.lower_sol
+        # print(lower_thresh.shape)
+        resource_index = budget_remaining - \
+            np.matmul(sizes, self.lower_sol) > 0
+        allocation = resource_index * lower_thresh + \
+            (1 - resource_index) * \
+            np.array([budget_remaining / np.sum(sizes), ]*self.num_types)
+        self.budget_remaining -= budget_remaining - \
+            np.matmul(sizes, allocation)
+
+        return allocation
 
     def pick_action(self, state, step):
         action = self.greedy(state, step)
--- or_suite/agents/resource_allocation/hope_guardrail.py
+++ or_suite/agents/resource_allocation/hope_guardrail.py
@@ -4,6 +4,8 @@ from .. import Agent
 
 
 ''' Agent which implements several heuristic algorithms'''
+
+
 class hopeguardrailAgent(Agent):
 
     def __init__(self, epLen, env_config, scale):
@@ -21,44 +23,46 @@ class hopeguardrailAgent(Agent):
         self.scale = scale
 
         #print('Starting Budget: ' + str(self.current_budget))
-        
+
         self.epLen = epLen
         self.data = []
         self.first_allocation_done = False
         self.exp_endowments, self.var_endowments = self.get_expected_endowments()
         self.prob, self.solver = self.generate_cvxpy_solver()
-        self.lower_sol = np.zeros((self.num_types,self.num_resources))
+        self.lower_sol = np.zeros((self.num_types, self.num_resources))
         self.upper_sol = np.zeros((self.num_types, self.num_resources))
         print('Mean and variance endomwnets:')
         print(self.exp_endowments, self.var_endowments)
 
-        #print("R")
-        #print(self.rel_exp_endowments)
+        # print("R")
+        # print(self.rel_exp_endowments)
 
     def generate_cvxpy_solver(self):
         """
         Creates a generic solver to solve the offline resource allocation problem
-        
-        Inputs: 
-            num_types - number of types
-            num_resources - number of resources
+
+        Args: 
+            num_types: number of types
+            num_resources: number of resources
         Returns:
             prob - CVXPY problem object
             solver - function that solves the problem given data
         """
         num_types = self.num_types
         num_resources = self.num_resources
-        x = cp.Variable(shape=(num_types,num_resources))
+        x = cp.Variable(shape=(num_types, num_resources))
         sizes = cp.Parameter(num_types, nonneg=True)
         weights = cp.Parameter((num_types, num_resources), nonneg=True)
         budget = cp.Parameter(num_resources, nonneg=True)
-        objective = cp.Maximize(cp.log(cp.sum(cp.multiply(x, weights), axis=1)) @ sizes)
+        objective = cp.Maximize(
+            cp.log(cp.sum(cp.multiply(x, weights), axis=1)) @ sizes)
         constraints = []
         constraints += [0 <= x]
         for i in range(num_resources):
             constraints += [x[:, i] @ sizes <= budget[i]]
         # constraints += [x @ sizes <= budget]
         prob = cp.Problem(objective, constraints)
+
         def solver(true_sizes, true_weights, true_budget):
             sizes.value = true_sizes
             weights.value = true_weights
@@ -66,28 +70,28 @@ class hopeguardrailAgent(Agent):
             prob.solve()
             return prob.value, np.around(x.value, 5)
         return prob, solver
-    
-    def get_lower_upper_sol(self,init_sizes):
+
+    def get_lower_upper_sol(self, init_sizes):
         """
         uses solver to get the lower and upper
         """
         budget = self.env_config['init_budget']
         weights = self.env_config['weight_matrix']
         n = self.env_config['num_rounds']
-        tot_size =  np.sum(self.exp_endowments[:,1:], axis=1)
+        tot_size = np.sum(self.exp_endowments[:, 1:], axis=1)
         future_size = init_sizes + tot_size
 
-
-        conf_bnd = np.sqrt(np.max(self.var_endowments, axis=1)*np.mean(self.exp_endowments, axis=1)*(n-1))
-
+        conf_bnd = np.sqrt(np.max(self.var_endowments, axis=1)
+                           * np.mean(self.exp_endowments, axis=1)*(n-1))
 
         # print(future_size)
-        
-        lower_exp_size = future_size*(1 + np.max(np.sqrt(conf_bnd) / future_size))
+
+        lower_exp_size = future_size * \
+            (1 + np.max(np.sqrt(conf_bnd) / future_size))
         _, lower_sol = self.solver(lower_exp_size, weights, budget)
 
-
-        c = (1 / (n**(self.scale)))*(1 +  np.max(np.sqrt(conf_bnd) / future_size)) -  np.max(np.sqrt(conf_bnd) / future_size)
+        c = (1 / (n**(self.scale)))*(1 + np.max(np.sqrt(conf_bnd) /
+                                                future_size)) - np.max(np.sqrt(conf_bnd) / future_size)
         # print('Scaling Constant')
         # print(c)
         upper_exp_size = future_size*(1 - c)
@@ -98,8 +102,7 @@ class hopeguardrailAgent(Agent):
         # print(budget)
         return lower_sol, upper_sol
 
-
-    def get_expected_endowments(self,N=1000):
+    def get_expected_endowments(self, N=1000):
         """
         Monte Carlo Method for estimating Expectation of type distribution using N realizations
         Only need to run this once to get expectations for all locations
@@ -129,13 +132,13 @@ class hopeguardrailAgent(Agent):
         self.current_budget = np.copy(self.env_config['init_budget'])
 
         self.data = []
-    
+
     def update_config(self, env, config):
         '''Updates environment configuration dictionary'''
         self.env_config = config
         return
 
-    def update_obs(self, obs, action, reward, newObs, timestep, info): 
+    def update_obs(self, obs, action, reward, newObs, timestep, info):
         '''Add observation to records'''
         self.data.append(newObs)
         return
@@ -143,7 +146,6 @@ class hopeguardrailAgent(Agent):
     def update_policy(self, k):
         '''Update internal policy based upon records'''
         self.current_budget = np.copy(self.env_config['init_budget'])
-
 
     def pick_action(self, state, step):
 
@@ -157,17 +159,16 @@ class hopeguardrailAgent(Agent):
             print('Lower and Upper Solutions:')
             print(self.lower_sol, self.upper_sol)
 
+        conf_bnd = np.sqrt(np.max(self.var_endowments, axis=1)
+                           * np.mean(self.exp_endowments, axis=1)*num_remaining)
 
-
-
-        conf_bnd = np.sqrt(np.max(self.var_endowments, axis=1)*np.mean(self.exp_endowments, axis=1)*num_remaining)
-
-
-        budget_required = budget_remaining - np.matmul(sizes, self.upper_sol) - np.matmul(np.sum(self.exp_endowments[:, (step+1):], axis=1) + conf_bnd, self.lower_sol) > 0
+        budget_required = budget_remaining - np.matmul(sizes, self.upper_sol) - np.matmul(
+            np.sum(self.exp_endowments[:, (step+1):], axis=1) + conf_bnd, self.lower_sol) > 0
         budget_index = budget_remaining - np.matmul(sizes, self.lower_sol) > 0
 
         allocation = budget_required * self.upper_sol \
-                + (1 - budget_required)*budget_index*self.lower_sol \
-                + (1 - budget_required) * (1 - budget_index) * np.array([budget_remaining / np.sum(sizes)])
-        
+            + (1 - budget_required)*budget_index*self.lower_sol \
+            + (1 - budget_required) * (1 - budget_index) * \
+            np.array([budget_remaining / np.sum(sizes)])
+
         return allocation
