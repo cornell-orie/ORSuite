329bbf18eae884500a2818a498ce9033dfdce59b
JiaoliB <59347932+JiaoliB@users.noreply.github.com>
2021-07-13 14:51:37 -0400

    fixed demo simulation file

--- ambulance_demo_simulation.py
+++ ambulance_demo_simulation.py
@@ -1,153 +1,119 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "45119d0b",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#from pyvirtualdisplay import Display\n",
-    "#display = Display(visible=0, size=(500, 800))\n",
-    "#display.start()\n",
-    "\n",
-    "import or_suite\n",
-    "import gym\n",
-    "import numpy as np\n",
-    "import matplotlib.pyplot as plt\n",
-    "from IPython import display as ipythondisplay\n",
-    "import rendering\n",
-    "\n",
-    "import time\n",
-    "import copy\n",
-    "\n",
-    "a = 5\n",
-    "b = 2\n",
-    "CONFIG = {'epLen': 5,\n",
-    "    'arrival_dist': lambda x : np.random.beta(a,b), \n",
-    "    'alpha': 0.25, \n",
-    "    'starting_state': np.array([0.0, 0.0]), \n",
-    "    'num_ambulance': 2,\n",
-    "    'norm': 1\n",
-    "}\n",
-    "\n",
-    "alpha = CONFIG['alpha']\n",
-    "epLen = CONFIG['epLen']\n",
-    "state = CONFIG['starting_state']\n",
-    "num_ambulance = CONFIG['num_ambulance']\n",
-    "\n",
-    "agent = or_suite.agents.ambulance.command_line_metric.commandLineAgent(epLen)\n",
-    "env = gym.make('Ambulance-v0', config=CONFIG)\n",
-    "env.reset()\n",
-    "\n",
-    "done = False\n",
-    "your_rewards = []\n",
-    "heuristic_agent_rewards = []\n",
-    "your_total_reward = 0\n",
-    "heuristic_agent_total_reward = 0\n",
-    "\n",
-    "median_est = (a - 1/3)/(a + b - 2/3)\n",
-    "heuristic_agent_states = [state]\n",
-    "\n",
-    "x_axis = ['Your Reward So Far', 'RL Algorithm Reward So Far']\n",
-    "\n",
-    "\n",
-    "def display_animation(screen, time_to_display):\n",
-    "    plt.imshow(screen)\n",
-    "    ipythondisplay.clear_output(wait=True)\n",
-    "    if time_to_display is not None:\n",
-    "        ipythondisplay.display(plt.gcf())\n",
-    "        time.sleep(time_to_display)\n",
-    "\n",
-    "def plot_rewards():\n",
-    "    fig = plt.figure()\n",
-    "    ax = fig.add_axes([0,0,1,1])\n",
-    "    y_axis = [your_total_reward, heuristic_agent_total_reward]\n",
-    "    ax.bar(x_axis, y_axis)\n",
-    "    plt.show()\n",
-    "\n",
-    "\n",
-    "while not done:\n",
-    "    action = agent.greedy(state, 0)\n",
-    "    \n",
-    "    state, reward, done, info = env.step(action)\n",
-    "    your_rewards.append(reward)\n",
-    "    your_total_reward += reward\n",
-    "\n",
-    "    # by comparing the final state with the action the user chose, we can figure out where the most recent arrival was\n",
-    "    previous_arrival_ind = np.argmax(np.abs(action - state))\n",
-    "    previous_arrival = state[previous_arrival_ind]\n",
-    "\n",
-    "    # the heuristic agent always chooses to put all the ambulances at the median estimate\n",
-    "    heuristic_agent_action = np.full(num_ambulance, median_est)\n",
-    "\n",
-    "    # the state will have one ambulance where the call arrived, and all other ambulances at the median estimate\n",
-    "    # doesn't matter which ambulance responds to the call because they're all at the same place\n",
-    "    heuristic_agent_state = np.concatenate([np.full(num_ambulance - 1, median_est), [previous_arrival]])\n",
-    "    heuristic_agent_states.append(heuristic_agent_state)\n",
-    "\n",
-    "    heuristic_agent_reward = -1 * (alpha * np.sum(np.abs(heuristic_agent_states[-2] - heuristic_agent_action)) + (1 - alpha) * np.sum(np.abs(heuristic_agent_action - heuristic_agent_state)))\n",
-    "    heuristic_agent_rewards.append(heuristic_agent_reward)\n",
-    "    heuristic_agent_total_reward += heuristic_agent_reward\n",
-    "    \n",
-    "    env.viewer = rendering.PygletWindow(850, 550)\n",
-    "    env.viewer.window.set_visible(False)\n",
-    "    screen1, screen2, screen3 = env.render(mode='rgb_array')\n",
-    "    \n",
-    "    # display each step of the environment for 2 seconds\n",
-    "    display_animation(screen1, 2)\n",
-    "    display_animation(screen2, 2)\n",
-    "    display_animation(screen3, None)\n",
-    "\n",
-    "    # plot your reward vs the agent's reward\n",
-    "    plot_rewards()\n",
-    "    time.sleep(2)\n",
-    "\n",
-    "    print(\"\\nThe most recent call arrival was at \" + str(previous_arrival) + \", and ambulance \" + str(previous_arrival_ind+1) + \" responded to the call.\\n\")\n",
-    "\n",
-    "    time.sleep(2)\n",
-    "    \n",
-    "    if not done:\n",
-    "        cont = input(\"Continue? [y/n]\")\n",
-    "        if cont == \"n\":\n",
-    "            done = True\n",
-    "            break\n",
-    "\n",
-    "\n",
-    "ipythondisplay.clear_output(wait=True)\n",
-    "env.close()\n",
-    "\n",
-    "if np.sum(your_rewards) > np.sum(heuristic_agent_rewards):\n",
-    "    print(\"CONGRATS! You beat the RL algorithm.\")\n",
-    "else:\n",
-    "    print(\"You did not get a better reward than the RL algorithm.\")\n",
-    "\n",
-    "print(\"\\nYour total reward over all iterations was \", round(sum(your_rewards),3))\n",
-    "print(\"The RL algorithm's total reward over all iterations was \", round(sum(heuristic_agent_rewards),3), \"\\n\")\n",
-    "\n",
-    "plot_rewards()"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
+#from pyvirtualdisplay import Display
+#display = Display(visible=0, size=(500, 800))
+#display.start()
+
+import or_suite
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import display as ipythondisplay
+import rendering
+
+import time
+import copy
+
+a = 5
+b = 2
+CONFIG = {'epLen': 5,
+    'arrival_dist': lambda x : np.random.beta(a,b), 
+    'alpha': 0.25, 
+    'starting_state': np.array([0.0, 0.0]), 
+    'num_ambulance': 2,
+    'norm': 1
 }
+
+alpha = CONFIG['alpha']
+epLen = CONFIG['epLen']
+state = CONFIG['starting_state']
+num_ambulance = CONFIG['num_ambulance']
+
+agent = or_suite.agents.ambulance.command_line_metric.commandLineAgent(epLen)
+env = gym.make('Ambulance-v0', config=CONFIG)
+env.reset()
+
+done = False
+your_rewards = []
+heuristic_agent_rewards = []
+your_total_reward = 0
+heuristic_agent_total_reward = 0
+
+median_est = (a - 1/3)/(a + b - 2/3)
+heuristic_agent_states = [state]
+
+x_axis = ['Your Reward So Far', 'RL Algorithm Reward So Far']
+
+
+def display_animation(screen, time_to_display):
+    plt.imshow(screen)
+    ipythondisplay.clear_output(wait=True)
+    if time_to_display is not None:
+        ipythondisplay.display(plt.gcf())
+        time.sleep(time_to_display)
+
+def plot_rewards():
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    y_axis = [your_total_reward, heuristic_agent_total_reward]
+    ax.bar(x_axis, y_axis)
+    plt.show()
+
+
+while not done:
+    action = agent.greedy(state, 0)
+    
+    state, reward, done, info = env.step(action)
+    your_rewards.append(reward)
+    your_total_reward += reward
+
+    # by comparing the final state with the action the user chose, we can figure out where the most recent arrival was
+    previous_arrival_ind = np.argmax(np.abs(action - state))
+    previous_arrival = state[previous_arrival_ind]
+
+    # the heuristic agent always chooses to put all the ambulances at the median estimate
+    heuristic_agent_action = np.full(num_ambulance, median_est)
+
+    # the state will have one ambulance where the call arrived, and all other ambulances at the median estimate
+    # doesn't matter which ambulance responds to the call because they're all at the same place
+    heuristic_agent_state = np.concatenate([np.full(num_ambulance - 1, median_est), [previous_arrival]])
+    heuristic_agent_states.append(heuristic_agent_state)
+
+    heuristic_agent_reward = -1 * (alpha * np.sum(np.abs(heuristic_agent_states[-2] - heuristic_agent_action)) + (1 - alpha) * np.sum(np.abs(heuristic_agent_action - heuristic_agent_state)))
+    heuristic_agent_rewards.append(heuristic_agent_reward)
+    heuristic_agent_total_reward += heuristic_agent_reward
+    
+    env.viewer = rendering.PygletWindow(850, 550)
+    env.viewer.window.set_visible(False)
+    screen1, screen2, screen3 = env.render(mode='rgb_array')
+    
+    # display each step of the environment for 2 seconds
+    display_animation(screen1, 2)
+    display_animation(screen2, 2)
+    display_animation(screen3, None)
+
+    # plot your reward vs the agent's reward
+    plot_rewards()
+    time.sleep(2)
+
+    print("\nThe most recent call arrival was at " + str(previous_arrival) + ", and ambulance " + str(previous_arrival_ind+1) + " responded to the call.\n")
+
+    time.sleep(2)
+    
+    if not done:
+        cont = input("Continue? [y/n]")
+        if cont == "n":
+            done = True
+            break
+
+
+ipythondisplay.clear_output(wait=True)
+env.close()
+
+if np.sum(your_rewards) > np.sum(heuristic_agent_rewards):
+    print("CONGRATS! You beat the RL algorithm.")
+else:
+    print("You did not get a better reward than the RL algorithm.")
+
+print("\nYour total reward over all iterations was ", round(sum(your_rewards),3))
+print("The RL algorithm's total reward over all iterations was ", round(sum(heuristic_agent_rewards),3), "\n")
+
+plot_rewards()
