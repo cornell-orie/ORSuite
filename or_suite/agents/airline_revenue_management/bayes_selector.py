9900cb029a271f8c44f267712c86ba7b8a916ac5
David Wolfers <dsw247@cornell.edu>
2021-11-12 15:09:59 -0500

    Added feasibility calculation

--- or_suite/agents/airline_revenue_management/bayes_selector.py
+++ or_suite/agents/airline_revenue_management/bayes_selector.py
@@ -18,23 +18,21 @@ class bayes_selectorAgent(Agent):
         ''' Update agent information based on the config__file'''
         self.config = config
         return
-        
+
     def reset(self):
         pass
-
 
     def update_obs(self, obs, action, reward, newObs, timestep, info):
         '''Adds newObs, the most recently observed state, to data
             adds the most recent call arrival, found in info['arrival'] to call_locs.'''
         return
 
-
     def pick_action(self, obs, timestep):
         '''Select an action based upon the observation'''
         # use the config to populate vector of the demands
         num_type = len(self.config['f'])
-        expect_type = np.sum(self.config['P'][timestep:,:],axis=0)
-            # gets the expected number of customer arrivals
+        expect_type = np.sum(self.config['P'][timestep:, :], axis=0)
+        # gets the expected number of customer arrivals
         x = cp.Variable(num_type)
         objective = cp.Maximize(self.config['f'].T @ x)
         constraints = []
@@ -48,7 +46,16 @@ class bayes_selectorAgent(Agent):
 
         # enforcing rounding rule here, add a trigger to do the other version somehow as well
         if self.round_flag:
-            action = np.asarray([1 if x.value[i] / expect_type[i] >= 1/2 else 0 for i in range(num_type)])
+           #  action = np.asarray(
+            # can also use np.less_equal
+           #      [1 if np.all(np.less(np.transpose(self.config['A'])[i], obs)) else 0 for i in range(num_type)])
+            action = np.asarray([1 if x.value[i] / expect_type[i] >= 1/2 and np.all(np.less_equal(
+                np.transpose(self.config['A'])[i], obs)) else 0 for i in range(num_type)])
+            # if action[i] == 1: make sure that np.less(np.transpose(self.config['A'])[i], obs) but with equal
+            # otherwise set to zero
+            # do for both round_flag = true or false
         else:
-            action = np.asarray([np.random.binomial(1, np.minimum(1, np.maximum(0, x.value[i] / expect_type[i])), size=None) for i in range(num_type)])
+            action = np.asarray([1 if np.random.binomial(1, np.minimum(1, np.maximum(
+                0, x.value[i] / expect_type[i])), size=None) == 1 and np.all(np.less_equal(
+                    np.transpose(self.config['A'])[i], obs)) else 0 for i in range(num_type)])
         return action
