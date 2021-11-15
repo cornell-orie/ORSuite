5233d981a207e6a4f91f8de35df3d0464b911f39
seanrsinclair <seanrsinclair@gmail.com>
2021-11-15 10:17:58 -0500

    updating inventory demo, adding new experiments, and todo under base surge

--- or_suite/agents/inventory_control_multiple_suppliers/base_surge.py
+++ or_suite/agents/inventory_control_multiple_suppliers/base_surge.py
@@ -6,27 +6,18 @@ from .. import Agent
 class base_surgeAgent(Agent):
 
     def __init__(self, r, S):
+        # TODO: Add documentation in the correct form, similar to the other algorithms outlining the definition of r and S and their interpretation
         self.r = r
         self.S = S
-
-        # S is the goal inventory level
 
     def update_config(self, env, config):
         ''' Update agent information based on the config__file'''
         self.config = config
-        lead_times = config['lead_times']
         if config['neg_inventory']:
             self.offset = config['max_inventory']
         else:
             self.offset = 0
         self.max_order = config['max_order']
-
-        # Doesn't include longest lead time (assuming lead times sorted in non-decreasing order)
-
-        # TODO: Figure out problem with action not being part of observation space
-        # Set up tests for 1 and 2 suppliers
-        # Run Stable Baselines (uncomment SB line)
-        # Look into linear programming solvers ( CVXPY, PuLP, or others)
 
     def pick_action(self, obs, h):
         '''Select an action based upon the observation'''
@@ -34,13 +25,10 @@ class base_surgeAgent(Agent):
         inventory = obs[-1] - self.offset
 
         order_amount = min(self.max_order, max(0, self.S - inventory))
-        # print(
-        #     f'Current order_amount: {order_amount} and inventory {inventory}')
+        # TODO: Max(0, asdf) important? Is it ever realized?
+        # adjust for the stuff which is currently coming in from previous lead time step?
 
-        # order_amount = min(self.max_order, min(
-        #     self.offset - 1, max(0, self.S - inventory)))
         action = np.asarray(self.r+[order_amount])
-        # action = [self.r, order_amount]
         return action
 
     def update_parameters(self, param):
