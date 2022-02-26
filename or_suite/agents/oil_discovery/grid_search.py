871349f4980e59d0cdc2cfbe67a2753f9b13d23f
jren44 <juntaoren44@gmail.com>
2022-02-26 14:48:58 -0500

    small edits to doc and initialization

--- or_suite/agents/oil_discovery/grid_search.py
+++ or_suite/agents/oil_discovery/grid_search.py
@@ -5,12 +5,12 @@ from .. import Agent
 
 class grid_searchAgent(Agent):
     """
-    TODO: Add description here
+    Agents that uses a bisection-method heuristic algorithm to find location of most oil.  
 
     Methods:
         reset() : clears data and call_locs which contain data on what has occurred so far in the environment
         update_config() : (UNIMPLEMENTED)
-        pick_action(state, step) : TODO add documentation
+        pick_action(state, step) : If upper and lower bounds set, move agent to midpoint. Else, perturb location.
 
     Attributes:
         epLen: (int) number of time steps to run the experiment for
@@ -20,11 +20,11 @@ class grid_searchAgent(Agent):
         """
         Args:
             epLen: (int) number of time steps to run the experiment for
-
+            dim: (int) dimension of metric space for agent and environment
         """
 
         # Saving parameters like the epLen, dimension of the space
-        self.epLen = epLen 
+        self.epLen = epLen
         self.dim = dim
 
         # Current bounds for the upper and lower estimates on where the maximum value is
@@ -32,14 +32,13 @@ class grid_searchAgent(Agent):
         self.lower = np.zeros((epLen, dim))
 
         # Estimates obtained for the "perturbed" values
-        self.perturb_estimates = np.zeros(epLen, 2*dim) # TODO: needs to depend on epLEn
+        self.perturb_estimates = np.zeros((epLen, 2*dim))
         self.midpoint_value = np.zeros(epLen)
-        self.dim_index = [0 for _ in range(self.epLen)] # TODO: Need to keep track of which dimension you are checking for the different steps
+        self.dim_index = [0 for _ in range(self.epLen)]
 
         # Indicator of "where" we are in the process, i.e. selecting the midpoint, doing small perturbations, etc
         self.eps = 1e-7
         self.select_midpoint = [True for _ in range(self.epLen)]
-
 
     def reset(self):
         # Resets data and call_locs arrays to be empty
@@ -49,19 +48,24 @@ class grid_searchAgent(Agent):
     def update_obs(self, obs, action, reward, newObs, timestep, info):
         '''Adds newObs, the most recently observed state, to data
             adds the most recent call arrival, found in info['arrival'] to call_locs.'''
-        if self.select_midpoint[timestep]: # If we selected the midpoint
-            self.midpoint_value[timestep] = reward # Store value of midpoint estimate
-            self.select_midpoint[timestep] = False # Switch to sampling the purturbed values
+        if self.select_midpoint[timestep]:  # If we selected the midpoint
+            # Store value of midpoint estimate
+            self.midpoint_value[timestep] = reward
+            # Switch to sampling the purturbed values
+            self.select_midpoint[timestep] = False
 
         else:
-            self.perturb_estimates[timestep, self.dim_index] = reward # stores the observed reward
+            # stores the observed reward
+            self.perturb_estimates[timestep, self.dim_index[timestep]] = reward
             self.dim_index[timestep] += 1
 
-            if self.dim_index[timestep] == 2*self.dim: # finished getting all the purturbed estimates
-                # TODO: Update the midpoint?
+            # finished getting all the purturbed estimates
+            if self.dim_index[timestep] == 2*self.dim:
                 # Get max value from the dim_list
                 # Figure out how to cut the upper and lower estimates, and continue?
-                self.upper[timestep] = self.upper[timestep] / 2 # just doing something stupid here for now
+
+                self.upper[timestep] = self.upper[timestep] / \
+                    2  # just doing something stupid here for now
                 self.dim_index[timestep] = 0
                 self.select_midpoint[timestep] = True
 
@@ -75,17 +79,16 @@ class grid_searchAgent(Agent):
         # Greedy algorithm does not update policy
         pass
 
-
     def pick_action(self, state, step):
         if self.select_midpoint[step]:
             action = (self.upper[step] + self.lower[step]) / 2
         else:
-            # One line calculation of purturbation I think?
+            # One line calculation of perturbation I think?
             # Gets the dimension index, mods it by 2 to get a 0,1 value, takes (-1) to the power
             # so the sign switches from positive and negative
-            # 
             p_location = np.zeros(self.dim)
             p_location[int(np.floor(self.dim_index / 2))] = 1
-            perturbation = np.zeros(self.dim) + (-1)**(np.mod(self.dim_index, 2))*p_location
-            action = (self.upper[step] + self.lower[step]) / 2
+            perturbation = np.zeros(
+                self.dim) + (-1)**(np.mod(self.dim_index, 2))*p_location
+            action = (self.upper[step] + self.lower[step]) / 2 + perturbation
         return action
