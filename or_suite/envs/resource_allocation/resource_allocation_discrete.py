d3b0b562965eea3220bac44b74c6e26cb426a6e8
logankraver <logan.kraver@gmail.com>
2022-04-18 14:31:51 -0400

    Registered Discrete Environment

--- or_suite/envs/resource_allocation/resource_allocation_discrete.py
+++ or_suite/envs/resource_allocation/resource_allocation_discrete.py
@@ -58,7 +58,7 @@ class DiscreteResourceAllocationEnvironment(gym.Env):
             [round(max(self.budget)) for _ in range(self.num_commodities*self.num_types)])
         # First K entries of observation space is the remaining budget, next is the number of each type at the location
         self.observation_space = spaces.MultiDiscrete(
-            [np.inf for _ in range(self.num_commodities+self.num_types)])
+            [round(max(self.budget)) for _ in range(self.num_commodities+self.num_types)])
 
     def reset(self):
         """
@@ -90,6 +90,7 @@ class DiscreteResourceAllocationEnvironment(gym.Env):
 
             info: dict; any additional information.
         """
+        assert self.action_space.contains(action)
 
         # subdividing state of (b,N) into the two components
         old_budget = self.state[:self.num_commodities]
