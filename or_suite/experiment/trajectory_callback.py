a61e2a311432a99e9ac08872e3e0df581388c022
David Wolfers <dsw247@cornell.edu>
2022-04-21 16:44:09 -0400

    callback and sb_experiment save trajectory

--- or_suite/experiment/trajectory_callback.py
+++ or_suite/experiment/trajectory_callback.py
@@ -15,21 +15,62 @@ class TrajectoryCallback(BaseCallback):
     Custom callback for plotting additional values in tensorboard.
 
     Args:
-      print: (bool) Print trajectory info to the console
+        verbose: (bool) Print trajectory info to the console
+
+    Attributes:
+        verbose: (int) 1 to print information, 0 to not print
+        trajectory: (list) A list of dictionaries to hold the trajectory information 
     """
 
     def __init__(self, verbose=0):
         self.verbose = verbose
         super(TrajectoryCallback, self).__init__(verbose)
+        self.trajectory = []
+        self.iteration_number = 0
 
     def _on_step(self) -> bool:
         reward = self.locals["rewards"][0]
         action = self.locals["actions"]
         info = self.locals["infos"]
         next_state = self.locals["new_obs"]
+        # if in initial state, have environment's starting state be the previous state
+        prev_state = self.trajectory[-1]["newState"] if not len(
+            self.trajectory) == 0 else self.locals["env"].get_attr("starting_state")
+
+        # Total number of steps in an episode
+        num_steps = self.locals["n_rollout_steps"]
+
+        # calculating current episode number
+        episode = int((len(self.trajectory) -
+                       self.iteration_number * self.locals["total_timesteps"]) / num_steps)
+        step = self.locals["n_steps"]
+
+        # iteration is done in outside loop for training, so it is not taken from self.locals
+        iteration = self.iteration_number
+
+        step_dict = {'iter': iteration,
+                     'episode': episode,
+                     'step': step,
+                     'oldState': prev_state,
+                     'action': action,
+                     'reward': reward,
+                     'newState': next_state,
+                     'info': info}
+        self.trajectory.append(step_dict)
+
         if self.verbose:
-            print('Reward: {}'.format(reward))
+            # print(self.locals)
+            print('Iteration: {}'.format(iteration))
+            print('Episode: {}'.format(episode))
+            print('Step: {}'.format(step))
+            print('Old State: {}'.format(prev_state))
             print('Action: {}'.format(action))
-            print('Next State: {}'.format(next_state))
+            print('Reward: {}'.format(reward))
+            print('New State: {}'.format(next_state))
             print('Info: {}'.format(info))
+            print('\n')
+
         return True
+
+    def update_iter(self):
+        self.iteration_number += 1
